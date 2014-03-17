d0efed852d9ab336d1ec543e7232589a38cc2c7c
Jesse Shawl <jesse@jshawl.com>
2014-03-17 15:56:29 -0400

    Add error handling for sass exception

--- app.rb
+++ app.rb
@@ -1,16 +1,21 @@
 require 'sinatra'
 require 'sass'
+set :environment, :production
 
 get '/' do
   erb :index
 end
 
 post '/compile' do
-  @sass = params[:sass]
-  sass = Sass::Engine.new(@sass, {
-    :syntax => :scss,
-    :style => :expanded
-  })
-  @css = sass.render
+  begin
+    @sass = params[:sass]
+    sass = Sass::Engine.new(@sass, {
+      :syntax => :scss,
+      :style => :expanded
+    })
+    @css = sass.render
+  rescue Sass::SyntaxError => e
+    @css = e
+  end
   erb :compile
 end
