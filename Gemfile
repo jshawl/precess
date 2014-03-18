8f4ef7aa5f5045c5d1fde688b611e5cf5ac4db44
Jesse Shawl <jesse@jshawl.com>
2014-03-18 11:09:11 -0400

    env vars not working

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 gem 'sinatra'
 gem 'sass'
+gem 'rest-client'
+gem 'json'
--- app.rb
+++ app.rb
@@ -1,8 +1,19 @@
 require 'sinatra'
 require 'sass'
+require 'rest-client'
+require 'json'
+
+#CLIENT_ID = '5cac1a56407e03f6be13'
+#CLIENT_SECRET = 'f816936b30905962b17fe826f7a60d8f2519dc30'
+
+CLIENT_ID = ENV['GH_BASIC_CLIENT_ID']
+puts 'CID'
+puts ENV
+CLIENT_SECRET = ENV['GH_BASIC_SECRET_ID']
+
 
 get '/' do
-  erb :index
+  erb :index, :locals => { :client_id => CLIENT_ID }
 end
 
 post '/compile' do
@@ -18,4 +29,15 @@ post '/compile' do
     @css = res
   end
   erb :compile
+end
+
+get '/callback' do
+  session_code = request.env['rack.request.query_hash']['code']
+  result = RestClient.post('https://github.com/login/oauth/access_token',
+                          {:client_id => CLIENT_ID,
+                           :client_secret => CLIENT_SECRET,
+                           :code => session_code},
+                           :accept => :json)
+  access_token = JSON.parse(result)['access_token']
+  puts access_token
 end
