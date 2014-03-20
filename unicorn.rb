21c00bee04889093712e5dd82078501edcdf730d
Jesse Shawl <jesse@jshawl.com>
2014-03-20 15:21:12 -0400

    idk WIP

--- unicorn.rb
+++ unicorn.rb
@@ -1,6 +1,6 @@
 # set path to app that will be used to configure unicorn, 
 # note the trailing slash in this example
-@dir = "/var/www/precess.co/"
+@dir = "/Users/jshawl/Sites/sasscade/"
 
 worker_processes 2
 working_directory @dir
